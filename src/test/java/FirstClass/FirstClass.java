dbe964524cf24925576ef864c7933fd16ea2ef4a
Atta Nasib <attanasib@gmail.com>
2022-02-03 18:06:08 -0500

    My first commit

---
+++ src/test/java/FirstClass/FirstClass.java
@@ -0,0 +1,18 @@
+package FirstClass;
+
+import org.testng.annotations.Test;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("I'm Atta");
+
+    }
+
+    @Test
+    public void FirstTest (){
+
+        System.out.println("My First Test");
+    }
+}
